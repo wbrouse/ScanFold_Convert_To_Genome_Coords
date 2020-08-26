a6faf315857e503b2da2e2f811412ac072c55422
wbrouse <60672421+wbrouse@users.noreply.github.com>
2020-08-26 14:09:48 -0500

    Update Transcript_to_Genomic_Coord.py

--- Transcript_to_Genomic_Coord.py
+++ Transcript_to_Genomic_Coord.py
@@ -1,7 +1,7 @@
 #ScanFold transcript coordinates to genome coordinates
 #This script will take three inputs, a .bp file from ScanFold, a starting genomic coordinate, and a chromosome number. This will convert transcript coordinates to genomic coordinates for easy comparison with annotation tracks from the UCSC genome browser
 
-#To run this script in the command line ensure script and bp file are in the working directory, call python, followed by script name, starting genomic coordinate, and chromosome written as 'Chr#'.
+#To run this script in the command line ensure script and bp file are in the working directory, call python, followed by script name, input file name, starting genomic coordinate, and chromosome written as 'Chr#'.
 #This will output a new ScanFold bp track that is in genomic coordinates and can be uploaded to IGV for visualization against other annotated tracks
 
 
